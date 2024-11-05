f409019b01237fd6c9d71ece214db96c54d76778
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-11-06 00:51:45 +0800

    feat(website): analytics & speedinsight

--- packages/website/src/app/(public)/_landing/recent-activity.tsx
+++ packages/website/src/app/(public)/_landing/recent-activity.tsx
@@ -23,11 +23,11 @@ export default function RecentlyPage({
       <div className={'grow flex flex-col max-w-[1024px] w-full mx-auto'}>
         <div className={"text-4xl lg:pt-14 lg:pb-7 pt-10 pb-4"}>最近</div>
         <div className={"grid grid-cols-1 lg:grid-cols-2 "} ref={containerRef}>
-          <Tabs defaultValue="spotify" className="w-full overflow-y-hidden h-[calc(100vh-160px)] lg:h-[calc(100vh-188px)] flex flex-col">
+          <Tabs defaultValue="github" className="w-full overflow-y-hidden h-[calc(100vh-160px)] lg:h-[calc(100vh-188px)] flex flex-col">
             <TabsList className="flex items-center justify-start w-auto gap-1 bg-transparent p-0 mt-0" ref={ref}>
+              <TabsTrigger value="github" className="data-[state=active]:bg-transparent bg-transparent px-1">最近在做</TabsTrigger>
               <TabsTrigger value="spotify" className="data-[state=active]:bg-transparent bg-transparent px-1">最近在听</TabsTrigger>
               <TabsTrigger value="steam" className="data-[state=active]:bg-transparent bg-transparent px-1">最近在玩</TabsTrigger>
-              <TabsTrigger value="github" className="data-[state=active]:bg-transparent bg-transparent px-1">最近在做</TabsTrigger>
               <TabsTrigger value="bilibili" className="data-[state=active]:bg-transparent bg-transparent px-1">最近在看</TabsTrigger>
             </TabsList>
               {
